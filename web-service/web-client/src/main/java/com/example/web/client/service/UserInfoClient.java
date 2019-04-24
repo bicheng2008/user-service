99de55cb58e60e0beb57af2b5035c4309a38d24b
王 <wang@123.com>
2019-04-24 17:50:42 +0900

    commit

--- web-service/web-client/src/main/java/com/example/web/client/service/UserInfoClient.java
+++ web-service/web-client/src/main/java/com/example/web/client/service/UserInfoClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("userapi")
+@FeignClient("userservice")
 @RequestMapping(value = "/userinfo")
 public interface UserInfoClient {
 
